13f0f674dd36468d658279def57ca1f9182dde51
Felix Aynhorn <fia73aynhorn@gso-koeln.de>
2019-06-21 20:22:47 +0200

    projectfiles
    Created first projectfiles

---
+++ Tante_Emma/Tante_Emma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tante_Emma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
